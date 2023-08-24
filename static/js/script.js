478d5b48f35ff7c4593b7b6e546f4faf352cd009
pmarbibi12 <marbibipanfilo@gmail.com>
2023-08-24 11:38:17 -0500

    more cleaning, changed fact section bullets

--- static/js/script.js
+++ static/js/script.js
@@ -16,6 +16,7 @@ function init() {
     })
     d3.json("/api/get_data").then( function(data) {
         displayResults(data, 1);
+        didYouKnow(data);
     });
 }
 
@@ -26,6 +27,10 @@ function displayResults(results, page) {
     
     let resultsList = d3.select("#result-list");
     resultsList.selectAll("*").remove();
+
+    let resultCount = d3.select("#results-count")
+    resultCount.selectAll("*").remove();
+    resultCount.append("p").text(`Results(${results.length}):`)
 
 
     pageResults.forEach(result => {
@@ -58,7 +63,7 @@ function displayResults(results, page) {
                 <p>Sex: ${sexChange(result.sex)}</p>
                 <p>Size: ${result.size}</p>
                 <p>Intake Type: ${result.intake_type}</p>
-                <p>In Date: ${result.in_date}</p>`
+                <p>In Date: ${new Date(parseDate(result.in_date)).toLocaleDateString()}</p>`
                 );
     });
 
@@ -177,7 +182,7 @@ function searchClicked(intakeType, petType, size, sex) {
                     <p>Sex: ${sexChange(result.sex)}</p>
                     <p>Size: ${result.size}</p>
                     <p>Intake Type: ${result.intake_type}</p>
-                    <p>In Date: ${result.in_date}</p>`
+                    <p>In Date: ${new Date(parseDate(result.in_date)).toLocaleDateString()}</p>`
                     );
             });
 
@@ -250,5 +255,159 @@ function getAge(petAge) {
         addChoice.text(`${petAge[i]}`).attr('value', `${petAge[i]}`)
     }
 };
+
+function didYouKnow(data){
+    let section = d3.select('#fact-content');
+    let [mostCommonDog, mostCommonOther] = commonDogAndOther(data);
+    let animalCounts = countAnimalTypes(data);
+    let { animal: longestStayAnimal, duration: longestStayDuration } = findLongestStayAnimal(data);
+    let oldestAnimal = findOldestAnimal(data);
+
+    // Construct the sentence dynamically using the counts and animal types
+    let animalCountBullet = "There are ";
+    let animalTypes = Object.keys(animalCounts);
+    for (let i = 0; i < animalTypes.length; i++) {
+        animalCountBullet += `${animalCounts[animalTypes[i]]} ${animalTypes[i].toLowerCase()}(s)`;
+        if (i === animalTypes.length - 2) {
+            animalCountBullet += " and ";
+        } else if (i < animalTypes.length - 1) {
+            animalCountBullet += ", ";
+        }
+    }
+    animalCountBullet += " in <u>Montgomery County Animal Services & Adoption Center</u>.";
+
+    let inDate = new Date(longestStayAnimal.in_date).toLocaleDateString();
+    let countDaysFromAnimalData = `<b>${longestStayAnimal.name}</b> has been in the <u>Montgomery County Animal Services & Adoption Center</u> since ${inDate}. That's ${longestStayDuration} days! Filter by "${longestStayAnimal.animal_type}" pet type to find them.`;
+
+    section.append('li').html(`The most common dog breed in <u>Montgomery County Animal Services & Adoption Center</u> is the "${mostCommonDog}."`);
+    section.append('li').text(`The most common "Other" breed type in the adoption center is the "${mostCommonOther.toLowerCase()}."`);
+    section.append('li').html(animalCountBullet);
+    section.append('li').html(countDaysFromAnimalData);
+    section.append('li').html(`The oldest animal in <u>Montgomery County Animal Services & Adoption Center</u> is <b>${oldestAnimal.name}</b>, aged ${oldestAnimal.age}. Filter by "${oldestAnimal.animal_type}" pet type to find them.`)
+
+}
+
+function commonDogAndOther(data) {
+    let breedCountsByAnimalType = {};
+
+
+    for (let i = 0; i < data.length; i++) {
+        let breed = data[i].breed;
+        let animalType = data[i].animal_type;
+
+        if (animalType === "DOG" || animalType === "OTHER") {
+            if (!breedCountsByAnimalType[animalType]) {
+                breedCountsByAnimalType[animalType] = {};
+            }
+
+            if (breed) {
+                if (breedCountsByAnimalType[animalType][breed]) {
+                    breedCountsByAnimalType[animalType][breed]++;
+                } else {
+                    breedCountsByAnimalType[animalType][breed] = 1;
+                }
+            }
+        }
+    }
+
+    let mostCommonBreedByType = {
+        "DOG": "",
+        "OTHER": ""
+    };
+
+    for (let animalType in breedCountsByAnimalType) {
+        let maxCount = 0;
+        let mostCommonBreed = "";
+
+        for (let breed in breedCountsByAnimalType[animalType]) {
+            let count = breedCountsByAnimalType[animalType][breed];
+            if (count > maxCount) {
+                maxCount = count;
+                mostCommonBreed = breed;
+            }
+        }
+
+        mostCommonBreedByType[animalType] = mostCommonBreed;
+    }
+
+    return [mostCommonBreedByType["DOG"],mostCommonBreedByType["OTHER"]];
+}
+
+function countAnimalTypes(data) {
+    let animalTypeCount = {};
+
+    for (let i = 0; i < data.length; i++) {
+        let animalType = data[i].animal_type;
+        if (animalType) {
+            if (animalTypeCount[animalType]) {
+                animalTypeCount[animalType]++;
+            } else {
+                animalTypeCount[animalType] = 1;
+            }
+        }
+    }
+
+    return animalTypeCount;
+}
+
+function findLongestStayAnimal(data) {
+    let longestStayAnimal = null;
+    let longestStayDuration = -1;
+
+    for (let i = 0; i < data.length; i++) {
+        let animal = data[i];
+        let inDate = parseDate(animal.in_date);
+        let currentDate = new Date();
+        let stayDuration = Math.floor((currentDate - inDate) / (1000 * 60 * 60 * 24)); // Calculate days
+
+        if (stayDuration > longestStayDuration) {
+            longestStayDuration = stayDuration;
+            longestStayAnimal = animal;
+        }
+    }
+
+    return {
+        animal: longestStayAnimal,
+        duration: longestStayDuration
+    };
+}
+
+function parseDate(dateString) {
+    let parts = dateString.split('-');
+    return new Date(parts[0], parts[1] - 1, parts[2].substr(0, 2));
+}
+
+function parseAge(ageString) {
+    let ageParts = ageString.split(' ');
+    let unit = ageParts[1].toUpperCase();
+    let value = parseInt(ageParts[0]);
+
+    if (unit === 'DAYS') {
+        return value;
+    } else if (unit === 'MONTHS') {
+        return value * 30; // Assuming 30 days in a month
+    } else if (unit === 'YEAR' || unit === 'YEARS') {
+        return value * 365; // Assuming 365 days in a year
+    } else {
+        return 0; // Return 0 for unrecognized units
+    }
+}
+
+function findOldestAnimal(data) {
+    let oldestAnimal = null;
+    let oldestAge = -1;
+
+    for (let i = 0; i < data.length; i++) {
+        let animal = data[i];
+        let age = parseAge(animal.age);
+
+        if (age > oldestAge) {
+            oldestAge = age;
+            oldestAnimal = animal;
+        }
+    }
+
+    return oldestAnimal;
+}
 
 init();
