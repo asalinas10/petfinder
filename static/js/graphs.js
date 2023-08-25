1eab719919af48bdfeb2ee40ab7f585ed00148b9
EBPaz <evapazderafit@gmail.com>
2023-08-24 20:27:29 -0500

    donut chart

--- static/js/graphs.js
+++ static/js/graphs.js
@@ -1,30 +1,33 @@
  //read in the data with d3
 function init() {
-    d3.json("https://data.montgomerycountymd.gov/api/views/e54u-qx42/rows.json?accessType=DOWNLOAD").then( function(data) {
-    type(data[12]);    
-    breeds(data[16]);
+    d3.json("https://data.montgomerycountymd.gov/api/views/e54u-qx42/rows.json?accessType=DOWNLOAD").then(function(data) {
+        getPiedata(data)
     });
 }
 
-type = []
-function loop(type) {
-    for (let i = 0; i < type.length; i++) {
-      console.log(type[i]);
+    // loop to get the data for each piece of the pie
+    for (let i = 0; i < data.length; i++) {
+        let breed = data[i].breed;
+        if(breed) {
+            if (breed[breed]) {
+                breed[breed]++;
+            }else {
+                breed[breed] = 1;
+            }
+        }
     }
-  }
 
 // Display the default plot
 function init() {
     let data = [{
-      values: values,
-      labels: labels,
+      values: Object.values(breed),
+      labels: Object.keys(breed),
+      domain: {column:0},
+      name: "Number of Animals by Breed",
+      hoverinfo: 'label+percent',
+      hole: .4,
       type: "pie"
     }];
-  
-    let layout = {
-      height: 600,
-      width: 800
-    };
   
     Plotly.newPlot("pie", data, layout);
   }
